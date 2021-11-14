8b71c6a11c0ca54da74998203398fb85010c8c3e
kanalveli R A <kanalveli.ramachandran@freshworks.com>
2021-11-14 23:41:58 +0530

    Chore: Lint fixes

--- tests/dummy/app/constants/notes.js
+++ tests/dummy/app/constants/notes.js
@@ -1,3 +1,4 @@
-const notes = 'The patronus charm - Incantation: "Expecto Patronum" - Concentrate - intense happy memory - ';
+const notes =
+  'The patronus charm - Incantation: "Expecto Patronum" - Concentrate - intense happy memory - ';
 
 export default notes;
--- tests/dummy/app/constants/spellwork.js
+++ tests/dummy/app/constants/spellwork.js
@@ -1,22 +1,27 @@
 export default [
   {
     name: 'Expelliarmus',
-    description: 'Almost beautiful in its simplicity, the Disarming Charm is frequently used in wizarding duels, forcing one’s opponent to drop their wand. As soon as a wizard casts this charm by bellowing ‘Expelliarmus’, a jet of red light shoots from their wand.'
+    description:
+      'Almost beautiful in its simplicity, the Disarming Charm is frequently used in wizarding duels, forcing one’s opponent to drop their wand. As soon as a wizard casts this charm by bellowing ‘Expelliarmus’, a jet of red light shoots from their wand.',
   },
   {
     name: 'Stupefy',
-    description: 'Stunning is one of the most useful spells in the wizard’s arsenal. It renders the victim unconscious. The effects of the Stunning Spell may be counteracted by ‘Rennervate’, but will also wear off with time on their own. Another method of avoiding the spell is to deflect it with a Shield Charm'
+    description:
+      'Stunning is one of the most useful spells in the wizard’s arsenal. It renders the victim unconscious. The effects of the Stunning Spell may be counteracted by ‘Rennervate’, but will also wear off with time on their own. Another method of avoiding the spell is to deflect it with a Shield Charm',
   },
   {
     name: 'Levicorpus',
-    description: 'The incantation ‘Levicorpus’ can be cast silently or spoken aloud, resulting in the victim being suspended in mid-air by their ankle. There is a counter jinx, called ‘Liberacorpus’.'
+    description:
+      'The incantation ‘Levicorpus’ can be cast silently or spoken aloud, resulting in the victim being suspended in mid-air by their ankle. There is a counter jinx, called ‘Liberacorpus’.',
   },
   {
     name: 'Reducto',
-    description: 'This spell splits a solid object into pieces or even into a fine dust.'
+    description:
+      'This spell splits a solid object into pieces or even into a fine dust.',
   },
   {
     name: 'Expecto Patronum',
-    description: 'The charm requires the incantation ‘Expecto Patronum’ combined with intense concentration on a single, extremely happy memory. The Patronus then acts as a shield, with Dementors feeding on it rather than the wizard himself. The spell also has another use, devised by Dumbledore. He invented a way of using the Patronus to deliver messages'
-  }
-]
+    description:
+      'The charm requires the incantation ‘Expecto Patronum’ combined with intense concentration on a single, extremely happy memory. The Patronus then acts as a shield, with Dementors feeding on it rather than the wizard himself. The spell also has another use, devised by Dumbledore. He invented a way of using the Patronus to deliver messages',
+  },
+];
